f353aab87c79cbf6fb3d3a55dc006cff47ee8886
sanjeevkumar46 <sanjeev.kumar133132@marwadiuniversity.ac.in>
2026-08-26 04:56:18 +0530

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "college";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// SQL to delete a record
+$sql = "DELETE FROM student WHERE id=2";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
