db6fc4ea02cfcecf707eda461cf4e1856fe240fe
sanjeevkumar46 <sanjeev.kumar133132@marwadiuniversity.ac.in>
2026-08-26 04:54:04 +0530

    Create file.php

---
+++ file.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<form action="uploader.php" method="post
+enctype="multipart/formd-data">
+ 
+ selectFile: <input type="file"name="fileToUpload"/>
+
+ <input type="submit" value="Upload Image"
+    name="submit"/>
+
+
+</form>
+</body>
+</html>
