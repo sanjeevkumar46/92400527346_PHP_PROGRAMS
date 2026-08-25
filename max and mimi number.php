af4d8e0a41bab4e74c52cf45366eaba711deff53
sanjeevkumar46 <sanjeev.kumar133132@marwadiuniversity.ac.in>
2026-08-26 04:41:43 +0530

    Create max and mimi number.php

---
+++ max and mimi number.php
@@ -0,0 +1,8 @@
+1.2 write a PHP program to find out maximum and minimum number.		
+<?php
+$a = 10;
+$b = 20;
+
+echo "Maximum Number = " . max($a, $b) . "<br>";
+echo "Minimum Number = " . min($a, $b);
+?>
